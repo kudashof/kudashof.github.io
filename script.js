76fee3bcca8aef1c8246b819ae1dad5b42f1f586
AlexKu <kudashof@gmail.com>
2019-12-06 04:48:35 +0300

    Реализация на Промисах, Загрузка, правки

--- script.js
+++ script.js
@@ -4,43 +4,87 @@ function apiSearch(event) {
     event.preventDefault();
     const searchText = document.querySelector('.form-control').value;
     const server = 'https://api.themoviedb.org/3/search/multi?api_key=ddd303084d05c0b61b51d539910ef146&language=ru&query=' + searchText;
-    requestApi('GET', server);
+    movie.innerHTML = 'Загрузка . . .';
+    requestApi(server)
+
+        .then(function (result) {
+            //спарсит JSON в объект
+            const output = JSON.parse(result);
+            let inner = '';
+
+            //вывод результата
+            output.results.forEach(function(item){
+                // выведит значение которое существует
+                let nameItem = item.name || item.title;
+                //add адаптив верстку к пустой строке
+                inner += `<div class="col-12 col-md-4 col-xl-3">${nameItem} + </div>`;
+            });
+
+            movie.innerHTML = inner;
+            console.log(output);
+    })
+        .catch()
+    ;
 }
 
 searchForm.addEventListener('submit', apiSearch);
 
 
 //подключаемся к Api
-function requestApi(method, url) {
+function requestApi(url) {
+    return new Promise(function (resolve, reject) {
+        const request = new XMLHttpRequest();
+        request.open('GET', url);
 
-    const request = new XMLHttpRequest();
-    //console.log(request);
-    request.open(method, url);
-    request.send();
-
-    request.addEventListener('readystatechange', () =>{
-        if (request.readyState !== 4) return;
-
-        if(request.status !==200){
-            console.log('error:' + request.status);
-            return;
-        }
-        //спарсит JSON в объект
-        const output = JSON.parse(request.responseText);
-
-        let inner = '';
-
-        //вывод результата
-        output.results.forEach(function(item){
-            // выведит значение которое существует
-            let nameItem = item.name || item.title;
-            console.log(nameItem);
-            //add верстку к пустой строке
-            inner += `<div class="col-12">${nameItem} + </div>`;
+        request.addEventListener('load', function () {
+            if (request.status !== 200) {
+                reject({
+                    status: request.status
+                });
+                return;
+            }
+            //ответ от сервера
+            resolve(request.response);
         });
-
-        movie.innerHTML = inner;
-
-        console.log(output);
+        request.addEventListener('error', function () {
+            reject({
+                status: request.status
+            });
+        });
+        request.send();
     });
 }
+
+
+
+
+
+    // request.addEventListener('readystatechange', () =>{
+    //     if (request.readyState !== 4) {
+    //         movie.innerHTML = 'Загрузка';
+    //         return;
+    //     }
+    //
+    //     if(request.status !==200){
+    //         movie.innerHTML = 'Упс, что то пошло не так!';
+    //         console.log('error:' + request.status);
+    //         return;
+    //     }
+    //      спарсит JSON в объект
+    //     const output = JSON.parse(request.responseText);
+    //
+    //     let inner = '';
+    //
+    //     //вывод результата
+    //     output.results.forEach(function(item){
+    //         // выведит значение которое существует
+    //         let nameItem = item.name || item.title;
+    //         console.log(nameItem);
+    //         //add адаптив верстку к пустой строке
+    //         inner += `<div class="col-12 col-md-4 col-xl-3">${nameItem} + </div>`;
+    //     });
+    //
+    //     movie.innerHTML = inner;
+    //
+    //     console.log(output);
+    // });
