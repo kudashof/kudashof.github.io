5b7af06d877d56d79877a5415660183d38a2b607
AlexKu <kudashof@gmail.com>
2019-12-18 05:40:18 +0300

    Выводит инфо: title, оф сайт и imdb страницу

--- fetchscript.js
+++ fetchscript.js
@@ -74,9 +74,9 @@ function showFullInfo() {
     //console.dir(this.dataset.type);
     let url = '';
     if(this.dataset.type === 'movie'){
-        url = 'https://api.themoviedb.org/3/movie/' + this.dataset.id + '?api_key=<<ddd303084d05c0b61b51d539910ef146>>&language=ru';
+        url = 'https://api.themoviedb.org/3/movie/' + this.dataset.id + '?api_key=ddd303084d05c0b61b51d539910ef146&language=ru';
     } else if(this.dataset.type ==='tv'){
-        url = 'https://api.themoviedb.org/3/tv/' + this.dataset.id + '?api_key=<<ddd303084d05c0b61b51d539910ef146>>&language=ru';
+        url = 'https://api.themoviedb.org/3/tv/' + this.dataset.id + '?api_key=ddd303084d05c0b61b51d539910ef146&language=ru';
     } else {
         movie.innerHTML = '<h2 class="col-12 text-center text-danger">Произошла ошибка, повторите позже</h2>';
     }
@@ -86,39 +86,23 @@ function showFullInfo() {
             if (value.status !== 200) {
                 return Promise.reject(new Error('Ошибка'))
             }
-            return value.json(); //возвращает promise
+            return value.json();
+            //возвращает promise
         })
         .then(function (output) {
-            // console.log(output);
-            let inner = '';
-            //сообщение если ничего не найдено по запросу
-            if(output.results.length === 0){
-                inner = '<h2 class="col-12 text-center text-info">По вашему запросу ничего не найдено</h2>';
-            }
-            //console.log(output.results.length);
-            //вывод результата
-            output.results.forEach(function (item) {
-                //console.log(item);
-                // выведит значение которое существует
-                // название фильма или сериала
-                let nameItem = item.name || item.title;
-                // постер, если нету заглушка
-                const poster = item.poster_path ? urlPoster + item.poster_path : './img/noposter.jpg';
-                let dataInfo = '';
-                //убирает лишних персонажей
-                if(item.media_type !=='person') dataInfo = `data-id="${item.id}" data-type="${item.media_type}"`
-                //add адаптив верстку к пустой строке
-                inner += `
-                    <div class="col-12 col-md-6 col-xl-3 item">
-                    <img src="${poster}" class="img_poster" alt="${nameItem}" ${dataInfo}>
-                    <h5>${nameItem}</h5>
+            console.log(output);
+            movie.innerHTML = `
+            <h4 class="col-12 text-center text-info">${output.name || output.title}</h4>
+            <div class="col-4">
+                <img src="${urlPoster + output.poster_path}" alt="${output.name || output.title}">
+                ${(output.homepage) ? `<p class="text-center"><a href="${output.homepage}"> Официальная страница </a></p>` :
+            ''}
+                ${(output.homepage) ? `<p class="text-center"><a href="https://imdb.com/title/${output.imdb_id}" target="_blank"> Страница на IMDB.com </a></p>` :
+                    ''}    
                     </div>
-                    `;
-            });
+                    <div class="col-8"></div>
+`;
 
-            movie.innerHTML = inner;
-
-            addEventMedia();
 
         })
         .catch(function (reason) {
